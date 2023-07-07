9a2a09d6267e8deac1d0c0cbfc5f1fbd2c5a6cd4
Siddhartha Borah <siddhartha.borah@rubrik.com>
2023-07-07 16:17:07 +0530

    Connect to mysql

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module golang-mysql-example
 
 go 1.19
+
+require github.com/go-sql-driver/mysql v1.7.1
--- src/main.go
+++ src/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// Create a MySQL database connection string
+	connectionString := "root:password@tcp(localhost:3306)/sample"
+
+	// Open a connection to the database
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Query the events table
+	rows, err := db.Query("SELECT * FROM events")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	// Iterate over the rows and print the values
+	for rows.Next() {
+		var id string
+		var name string
+		var description string
+		var eventDate string
+		err := rows.Scan(&id, &name, &description, &eventDate)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("ID: %s, Name: %s, Description: %s, Date: %s\n", id, name, description, eventDate)
+	}
+
+	// Check for any errors occurred during iteration
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
